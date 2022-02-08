d69250a8adbdd6fc1f9d92d6aa461660ddaa7e48
GeekBrains <kubasovae@mail.ru>
2022-02-09 00:28:28 +0400

    Task4

---
+++ arithOperations.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//арифметическая операция +
+function SumOperation(arg1, arg2) {
+    return (arg1 + arg2);
+}
+
+//арифметическая операция -
+function DiffOperation(arg1, arg2) {
+    return (arg1 - arg2);
+}
+
+//арифметическая операция *
+function MultiOperation(arg1, arg2) {
+    return (arg1 * arg2);
+}
+
+//арифметическая операция /
+function DivOperation(arg1, arg2) {
+    return (arg1 / arg2);
+}
